f2a28726cbfc4ef6f32566afa0d6d664e44479eb
kingston <kingstonjohn84@gmail.com>
2026-09-16 12:23:58 +0530

    Test no1

---
+++ new.js
@@ -0,0 +1,77 @@
+// let name="king"
+// console.log("hhh");
+
+// var num=1;
+// console.log(name1);
+// var name1="hhghh";
+// name1=55;
+// name1="king"
+
+
+// const place="kkkkk";
+// // place="ggggg";
+// console.log(place);
+
+
+
+// if(false){
+//     console.log("hghghg");
+    
+// }
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+    
+// }
+
+// let skills= ["JavaScript", "HTML", "CSS"];
+// console.log(skills[2]);
+
+
+// let user = {
+//     name: "king",
+//     age: 25,
+//     city: "Madurai"
+// };
+
+// console.log(user.city);
+
+// function handleName(a,b){
+//     let sum=a+b;
+//     console.log(sum);
+    
+    
+// }
+// let handlePlace=function(){
+//     console.log("hghg");
+    
+// };
+
+// let handleCar=y=>console.log(y);
+//     let bike=(i)=>{
+//         console.log(i)
+//     }
+// bike("text")
+// handleCar(7)
+
+// handleName(3,5);
+// handleName(55,55)
+
+let name2="kingston"
+console.log("john");
+console.log(name2)
+let age = 20;
+if (age >= 18) {
+    console.log("You are eligible to vote");
+}
+let marks = 75;
+
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks <= 60) {
+    console.log("Grade B");
+} else if (marks >= 40) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
